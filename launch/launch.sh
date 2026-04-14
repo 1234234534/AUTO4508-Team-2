466f50fdc80f47000e9c5100d947b8e6bcc97c06
Jason <23958038@student.uwa.edu.au>
2026-04-14 17:01:19 +0800

    added mode_publisher_node to launch.sh

--- launch/launch.sh
+++ launch/launch.sh
@@ -4,5 +4,6 @@ source install/setup.bash
 ros2 run joy joy_node --ros-args -p device:=/dev/input/js0 &
 ros2 run teleop_twist_joy teleop_node --ros-args -p enable_button:=0 &
 ros2 run ariaNode ariaNode -rp /dev/ttyUSB0 &
+ros2 run mode_publisher_package mode_publisher_node &
 
 wait

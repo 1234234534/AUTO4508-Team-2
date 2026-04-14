cbc5480440a1c6a1d2b63681fd14d8d408864336
Jason <23958038@student.uwa.edu.au>
2026-04-14 12:29:33 +0800

    launch file trial 1

--- launch/launch.sh
+++ launch/launch.sh
@@ -1,6 +1,6 @@
 colcon build
 source install/setup.bash
 
-ros2 run joy joy_node &
-ros2 run teleop_twist_joy teleop_node &
+ros2 run joy joy_node --ros-args -p device:=/dev/input/js0 &
+ros2 run teleop_twist_joy teleop_node --ros-args -p enable_button:=0 &
 ros2 run ariaNode ariaNode

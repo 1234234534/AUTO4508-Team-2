dcc291ac1324337b19c77161ecff0859e1f3d350
Jason <23958038@student.uwa.edu.au>
2026-04-17 13:55:20 +0800

    added main_drive_node to launch file

--- launch/launch.sh
+++ launch/launch.sh
@@ -5,6 +5,7 @@ ros2 run joy joy_node --ros-args -p device:=/dev/input/js0 &
 ros2 run teleop_twist_joy teleop_node --ros-args -p require_enable_button:=false -r /cmd_vel:=cmd_vel_teleop &
 ros2 run ariaNode ariaNode -rp /dev/ttyUSB0 &
 ros2 run mode_publisher_package mode_publisher_node &
+ros2 run main_drive_package main_drive_node &
 ros2 launch sick_scan_xd sick_tim_5xx. launch.py &
 
 wait

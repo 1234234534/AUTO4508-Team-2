79f77f4314071c4b2b05b7a1515ab4fe5d9f0061
Jason <23958038@student.uwa.edu.au>
2026-04-17 14:18:03 +0800

    switched teleop_twist_joy configuration

--- launch/launch.sh
+++ launch/launch.sh
@@ -2,7 +2,14 @@ colcon build
 source install/setup.bash
 
 ros2 run joy joy_node --ros-args -p device:=/dev/input/js0 &
-ros2 run teleop_twist_joy teleop_node --ros-args -p require_enable_button:=false -r /cmd_vel:=cmd_vel_teleop &
+ros2 run teleop_twist_joy teleop_node \
+    --ros-args \
+    -p require_enable_button:=false \
+    -p axis_linear.x:=1 \
+    -p scale_linear.x:=0.5 \
+    -p axis_angular.yaw:=0 \
+    -p scale_angular.yaw:=-1.0 \
+    -r /cmd_vel:=cmd_vel_teleop &
 ros2 run ariaNode ariaNode -rp /dev/ttyUSB0 &
 ros2 run mode_publisher_package mode_publisher_node &
 ros2 run main_drive_package main_drive_node &

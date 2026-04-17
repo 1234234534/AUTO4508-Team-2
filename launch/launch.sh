e2d41a44d358f457e06fcbf86c205ad85aac71fd
Jason <23958038@student.uwa.edu.au>
2026-04-17 13:46:59 +0800

    main drive node draft 1

--- launch/launch.sh
+++ launch/launch.sh
@@ -2,8 +2,9 @@ colcon build
 source install/setup.bash
 
 ros2 run joy joy_node --ros-args -p device:=/dev/input/js0 &
-ros2 run teleop_twist_joy teleop_node --ros-args -p require_enable_button:=false &
+ros2 run teleop_twist_joy teleop_node --ros-args -p require_enable_button:=false -r /cmd_vel:=cmd_vel_teleop &
 ros2 run ariaNode ariaNode -rp /dev/ttyUSB0 &
 ros2 run mode_publisher_package mode_publisher_node &
+ros2 launch sick_scan_xd sick_tim_5xx. launch.py &
 
 wait
--- src/main_drive_package/main_drive_package/main_drive_node.py
+++ src/main_drive_package/main_drive_package/main_drive_node.py
@@ -1,6 +1,63 @@
-def main():
-    print('Hi from main_drive_package.')
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
+class MainDriveNode(Node):
 
-if __name__ == '__main__':
-    main()
+    def __init__(self):
+        super().__init__('main_drive_node')
+
+        # Store Current Mode
+        self.mode = "MANUAL"
+        self.allow = "OFF"
+
+        # Subscribe to mode, teleop_cmd_vel, (and nav2_cmd_vel later too)
+        self.subscription = self.create_subscription(String, '/mode', self.mode_callback, 10)
+        self.subscription2 = self.create_subscription(Twist, '/cmd_vel_teleop', self.teleop_callback, 10)
+
+        # Publish to cmd_vel
+        self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
+
+        # Store teleop cmd_vel (Later store nav2 cmd_vel too)
+        self.teleop_cmd = Twist()
+
+    def mode_callback(self, msg):
+        # Store current settings
+        self.mode, self.allow = msg.data.split(":")
+
+    def teleop_callback(self, msg):
+        # Store teleop_twist_joy msg
+        self.teleop_cmd = msg
+
+        #Update cmd_vel
+        self.update_output()
+
+    def update_output(self):
+        # Twist Message
+        twist = Twist()
+
+        if self.mode == "MANUAL":
+            # Activate Teleop_twist_joy
+            twist = self.teleop_cmd
+
+        elif mode == "AUTO" and allow == "ON":
+            # Set to 0.2 forward in x direction
+            twist.linear.x = 0.2
+            tiwst.angular.z = 0.0
+            #LATER
+            #twist = self.nav2_cmd
+
+        else:
+            # Set to 0 speed
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
+
+        self.publisher.publish(twist)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MainDriveNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()

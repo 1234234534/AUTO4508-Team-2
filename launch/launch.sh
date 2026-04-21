03d804353a978daa99883df35c5c5f1edfa27497
Jason <23958038@student.uwa.edu.au>
2026-04-21 09:01:27 +0800

    ros launch file changes

--- launch/launch.sh
+++ launch/launch.sh
@@ -1,6 +1,7 @@
 colcon build
 source install/setup.bash
 
+# Controller
 ros2 run joy joy_node --ros-args -p device:=/dev/input/js0 &
 ros2 run teleop_twist_joy teleop_node \
     --ros-args \
@@ -10,14 +11,24 @@ ros2 run teleop_twist_joy teleop_node \
     -p axis_angular.yaw:=0 \
     -p scale_angular.yaw:=1.0 \
     -r /cmd_vel:=cmd_vel_teleop &
+
+# Aria
 ros2 run ariaNode ariaNode -rp /dev/ttyUSB0 &
-ros2 run mode_publisher_package mode_publisher_node &
-ros2 run main_drive_package main_drive_node &
+sleep 2
+
+#Devices
+ros2 launch depthai_ros_driver camera.launch.py &
+sleep 2
+
 ros2 launch sick_scan_xd sick_tim_5xx. launch.py &
 ros2 run nmea_navsat_driver nmea_serial_driver \
     --ros-args \
     -p port:=/dev/ttyACM0 \
     -r fix:=/gps/fix &
-ros2 launch depthai_ros_driver camera.launch.py &
+
+
+# Our Nodes
+ros2 run mode_publisher_package mode_publisher_node &
+ros2 run main_drive_package main_drive_node &
 
 wait

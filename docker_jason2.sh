53bf6e51e377632515fd9625444aa41c7947a6bc
Jason <23958038@student.uwa.edu.au>
2026-04-21 08:17:07 +0800

    got rid of trying to display image (technically unnecessary)

--- docker_jason2.sh
+++ docker_jason2.sh
@@ -2,8 +2,6 @@ docker stop JASON || true
 docker rm JASON || true
 
 docker run -d --name JASON --network host --privileged team2-jason tail -f /dev/null \
-    -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
 && docker cp . JASON:/workspace \
 && docker exec -it JASON bash \
 && docker rm -f JASON

6ef583c7c8245970b50fe5d1209603d479a98f23
Jason <23958038@student.uwa.edu.au>
2026-04-21 08:39:24 +0800

    docker launch file edits for camera

--- docker_jason2.sh
+++ docker_jason2.sh
@@ -1,7 +1,11 @@
 docker stop JASON || true
 docker rm JASON || true
 
-docker run -d --name JASON --network host --privileged team2-jason tail -f /dev/null \
+docker run -d --name JASON \
+    --network host \
+    --privileged \
+    --device=/dev/bus/usb \
+    team2-jason tail -f /dev/null \
+    
 && docker cp . JASON:/workspace \
-&& docker exec -it JASON bash \
-&& docker rm -f JASON
+&& docker exec -it JASON bash

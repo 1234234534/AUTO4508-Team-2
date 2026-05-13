c3536880a2e3fd949ae4557723a8b6ba5dd496c8
Jason <23958038@student.uwa.edu.au>
2026-05-13 09:59:44 +0800

    No more GPS launch, No more point and shoot node, running odom node again

--- launch/launch.sh
+++ launch/launch.sh
@@ -18,17 +18,14 @@ ros2 run ariaNode ariaNode -rp /dev/ttyUSB0 &
 #Devices
 ros2 launch depthai_ros_driver camera.launch.py device_id:=19443010C11F481300 &
 #ros2 launch sick_scan_xd sick_tim_5xx. launch.py &
-ros2 run nmea_navsat_driver nmea_serial_driver \
-    --ros-args \
-    -p port:=/dev/ttyACM0 \
-    -r fix:=/gps/fix &
+#ros2 run nmea_navsat_driver nmea_serial_driver --ros-args -p port:=/dev/ttyACM0 -r fix:=/gps/fix &
 ros2 launch phidgets_spatial spatial-launch.py &
 
 # Our Nodes
 ros2 run mode_publisher_package mode_publisher_node &
 ros2 run main_drive_package main_drive_node &
-#ros2 run main_drive_package odom_node &
-ros2 run main_drive_package pointandshoot_node &
+ros2 run main_drive_package odom_node &
+#ros2 run main_drive_package pointandshoot_node &
 
 # Record Journey
 ros2 bag record /gps/fix /imu/mag /cmd_vel_pointandshoot /cmd_vel -o journey_$(date +%Y%m%d_%H%M%S) &

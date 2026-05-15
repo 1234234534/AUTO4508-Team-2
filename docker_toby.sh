b71106dbb49dcab190211875772527c26f908570
Jason <23958038@student.uwa.edu.au>
2026-05-15 12:27:36 +0800

    docker launch file changes for toby and winton

--- docker_toby.sh
+++ docker_toby.sh
@@ -1,12 +1,19 @@
 docker stop TOBY || true
 docker rm TOBY || true
 
+xhost +local:docker
+
 docker run -d --name TOBY \
     --network host \
     --privileged \
     --device=/dev/bus/usb \
     --device=/dev/ttyUSB0 \
     --device=/dev/ttyAMC0 \
+    --ipc=host \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $HOME/.Xauthority:/root/.Xauthority:rw \
     -v /dev:/dev \
     -v /sys:/sys \
     -v /run/udev:/run/udev \

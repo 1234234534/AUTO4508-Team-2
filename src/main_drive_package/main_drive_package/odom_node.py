8a7df5d73cce8702bc1d2477f128acae9ec44f13
Jason <23958038@student.uwa.edu.au>
2026-05-15 11:51:34 +0800

    fixed an error where odom node had base_link instead of base_footprint

--- src/main_drive_package/main_drive_package/odom_node.py
+++ src/main_drive_package/main_drive_package/odom_node.py
@@ -53,7 +53,7 @@ class OdomNode(Node):
 
         t.header.stamp = odom.header.stamp
         t.header.frame_id = "odom"
-        t.child_frame_id = "base_link"
+        t.child_frame_id = "base_footprint"
         t.transform.translation.x = msg.x
         t.transform.translation.y = msg.y
         t.transform.translation.z = 0.0

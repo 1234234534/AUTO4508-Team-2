e4a5c9ec247bd12c133b2713f479aa4e6bfd9e2b
Jason <23958038@student.uwa.edu.au>
2026-05-16 16:15:49 +0800

    tf work:

--- launch/launch.sh
+++ launch/launch.sh
@@ -23,10 +23,10 @@ ros2 launch depthai_ros_driver camera.launch.py device_id:=19443010C11F481300 &
 #ros2 run nmea_navsat_driver nmea_serial_driver --ros-args -p port:=/dev/ttyACM0 -r fix:=/gps/fix &
 ros2 launch phidgets_spatial spatial-launch.py &
 #ip addr add 192.168.198.1/24 dev enp89s0 2>/dev/null || true
-ros2 launch lakibeam1 lakibeam1_scan.launch.py & #--ros-args --log-level fatal &
+ros2 launch lakibeam1 lakibeam1_scan.launch.py &
 
 #TF Tree Static Transformations
-ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 base_footprint base_link &
+#ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 base_footprint base_link &
 ros2 run tf2_ros static_transform_publisher 0.2 0 0 0 0 0 base_link oak-d-base-frame &
 ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 base_link laser &
 
@@ -38,7 +38,7 @@ ros2 run main_drive_package odom_node &
 #ros2 run main_drive_package pointandshoot_node &
 
 #SLAM
-ros2 launch slam_toolbox online_async_launch.py & #slam_params_file:=$(pwd)/src/pioneer_robot/config/slam_params.yaml &
+ros2 launch slam_toolbox online_async_launch.py slam_params_file:=$(pwd)/src/pioneer_robot/config/slam_params.yaml &
 #rviz2
 rviz2 -d rviz2_config.rviz &
 

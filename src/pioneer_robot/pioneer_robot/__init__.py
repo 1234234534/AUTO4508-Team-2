3be278bafb03ea966674b5d5d3fbd51c14b29ed4
Jason <23958038@student.uwa.edu.au>
2026-05-22 03:58:12 +0800

    removed snapshot stuff that somehow stayed after revert

--- src/pioneer_robot/pioneer_robot/__init__.py
+++ src/pioneer_robot/pioneer_robot/__init__.py
@@ -6,4 +6,5 @@ self._snapshot_client = self.create_client(
     '/rosbag2_recorder/snapshot' # VERIFY ACTUAL TOPIC WHEN RUNNING
 )
 
-self._estop_snapshot_armed = True"""
+self._estop_snapshot_armed = True
+"""
